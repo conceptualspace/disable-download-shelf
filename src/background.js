87e9babe304b20027f2bda747ad17c29c7a23759
conceptualspace <dev@conceptualspace.net>
2022-04-05 16:11:58 -0400

    manifest v3

--- src/background.js
+++ src/background.js
@@ -1,22 +1,17 @@
 'use strict';
 
-// web extensions polyfill for ff/chrome/brave
-window.browser = (function () {
-    return window.browser || window.chrome;
-})();
+// show downloads page when toolbar button clicked
+chrome.action.onClicked.addListener(function(activeTab){
+  var newURL = "chrome://downloads";
+  chrome.tabs.create({ url: newURL });
+});
 
 function disableShelf() {
-	if (browser.downloads.setShelfEnabled) {
-		browser.downloads.setShelfEnabled(false);
+	if (chrome.downloads.setShelfEnabled) {
+		chrome.downloads.setShelfEnabled(false);
 	}
 }
 
 chrome.runtime.onInstalled.addListener(disableShelf);
 chrome.runtime.onStartup.addListener(disableShelf);
 chrome.windows.onCreated.addListener(disableShelf);
-
-// show downloads page when toolbar button clicked
-browser.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "chrome://downloads";
-  chrome.tabs.create({ url: newURL });
-});
